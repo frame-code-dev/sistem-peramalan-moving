3761a7b39be0912c5527efc4153757d66957f532
29rifjanj <rjndla@gmail.com>
2024-06-12 15:44:22 +0700

    feat: auth with dashboard

---
+++ database/migrations/2024_06_12_065015_create_pemasukan_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemasukan', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->nullable();
+            $table->string('keterangan')->nullable();
+            $table->double('nominal',13,2)->nullable();
+            $table->date('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemasukan');
+    }
+};
